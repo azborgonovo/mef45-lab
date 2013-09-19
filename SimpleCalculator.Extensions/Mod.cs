db34c2e5ebd71323c4209c40949ed945e970b0dc
André <azborgonovo@gmail.com>
2013-09-19 00:02:43 -0300

    Initial

---
+++ SimpleCalculator.Extensions/Mod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Extensions
+{
+    [Export(typeof(SimpleCalculator.IOperation))]
+    [ExportMetadata("Symbol", '%')]
+    public class Mod : IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left % right;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        private CompositionContainer _container;
+
+        [Import(typeof(ICalculator))]
+        public ICalculator calculator;
+
+        private Program()
+        {
+            // An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog();
+
+            // Adds all the parts found in the same assembly as the Program class
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            catalog.Catalogs.Add(new DirectoryCatalog(@"..\..\..\SimpleCalculator.Extensions\bin\Debug"));
+
+            // Create the CompositionContainer with the parts in the catalog
+            _container = new CompositionContainer(catalog);
+
+            // Fill the imports of this object
+            try
+            {
+                this._container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //Composition is performed in the constructor
+            String s;
+            Console.WriteLine("Enter Command:");
+            while (true)
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(p.calculator.Calculate(s));
+            }
+        }
+    }
+}
